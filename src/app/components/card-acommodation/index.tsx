a1834921d9253cb1d6240e796a225f930a8a8483
johanso <johanricardo4@gmail.com>
2024-01-07 22:19:45 -0500

    advances cards acommodation

--- src/app/components/card-acommodation/index.tsx
+++ src/app/components/card-acommodation/index.tsx
@@ -1,31 +1,46 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from './card-acommodation.module.scss'
 import Button from '../button'
-import { IconLocation, IconSea, IconStar } from '../icons'
+import { IconChatLeftText, IconDash, IconLocation, IconPlus, IconSea, IconStar } from '../icons'
 
 const CardAcommodation = () => {
+
+  const [activeAmenities, setActiveAmenities] = useState<boolean>(false)
+
   return (
     <article className={styles.cardAcommodation} data-type='carrusel-acommodation'>
       <header className={styles.cardAcommodation__header}>
         <div className={styles.cardAcommodation__stars}>
-          <IconStar size={16} color="#ffb900" />
-          <IconStar size={16} color="#ffb900" />
-          <IconStar size={16} color="#ffb900" />
-          <IconStar size={16} color="#ffb900" />
+          <IconStar size={14} color="#ffb900" />
+          <IconStar size={14} color="#ffb900" />
+          <IconStar size={14} color="#ffb900" />
+          <IconStar size={14} color="#ffb900" />
         </div>
         <Image src="/hero-1.jpg" alt="hero" width={200} height={250} />
       </header>
       <div className={styles.cardAcommodation__content}>
         <h3 className={styles.cardAcommodation__title}>San Andres 1</h3>
         <p className={styles.cardAcommodation__location}>
-          <span><IconLocation size={12} /> 2,1 km del centro</span> 
-          <span><IconSea size={16} /> A 350 m de la playa</span>
+          <span><IconLocation size={14} /> 2,1 km del centro</span> 
+          <span><IconSea size={18} /> A 350mts. de la playa</span>
         </p>
-        <p className={styles.cardAcommodation__description}>
-          San Andres, Costa Rica
+        <p className={styles.cardAcommodation__reputation}>
+          <span><IconChatLeftText size={14} /> 299 comentarios</span> 
+          <span><IconStar size={14} /> 8.0 Promedio de calificación</span>
         </p>
-        <Button color="primary" size="medium" icon='/icons/arrow-right.svg' text="Elegir Alojamiento" />
+        <div className={styles.cardAcommodation__amenities}>
+          <ul className={styles.cardAcommodation__amenities_list}>
+            <li className={styles.cardAcommodation__amenities_item}>Servicio 1</li>
+            <li className={styles.cardAcommodation__amenities_item}>Servicio 2</li>
+            <li className={styles.cardAcommodation__amenities_item}>Servicio 3</li>
+            <li className={styles.cardAcommodation__amenities_item}>Servicio 4</li>
+          </ul>
+        </div>
       </div>
+      <footer className={styles.cardAcommodation__footer}>
+        <Button color="primary" size="medium" icon='/icons/arrow-right.svg' text="Elegir Alojamiento" />
+      </footer>
     </article>
   )
 }
--- src/app/components/icons/index.tsx
+++ src/app/components/icons/index.tsx
@@ -44,6 +44,7 @@ export const IconQuote: React.FC<IconProps> = ({size = 16, color = 'currentColor
     </svg>
   )
 }
+
 export const IconArrowRight: React.FC<IconProps> = ({size = 16, color = 'currentColor'}) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} className="bi bi-arrow-right" viewBox="0 0 16 16">
@@ -51,10 +52,36 @@ export const IconArrowRight: React.FC<IconProps> = ({size = 16, color = 'current
     </svg>
   )
 }
+
 export const IconArrowLeft: React.FC<IconProps> = ({size = 16, color = 'currentColor'}) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} className="bi bi-arrow-left" viewBox="0 0 16 16">
       <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+    </svg>
+  )
+}
+
+export const IconPlus: React.FC<IconProps> = ({size = 16, color = 'currentColor'}) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} className="bi bi-plus-lg" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+    </svg>
+  )
+}
+
+export const IconDash: React.FC<IconProps> = ({size = 16, color = 'currentColor'}) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} className="bi bi-dash-lg" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"/>
+    </svg>
+  )
+}
+
+export const IconChatLeftText: React.FC<IconProps> = ({size = 16, color = 'currentColor'}) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} className="bi bi-chat-left-text" viewBox="0 0 16 16">
+      <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+      <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
     </svg>
   )
 }
